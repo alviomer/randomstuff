e2d2a618dee7a14e79c8aa72a4e75dfe7df2f03e
omeralvi <omer.a@28bysamwood.com>
2022-08-21 21:01:41 +1000

    code refactor

---
+++ game_data.py
@@ -0,0 +1,27 @@
+
+new_game_string = "============================================ New Game ============================================"
+start_game_str = "============================================ Start Game ============================================ "
+end_game_str = "============================================ End Game   ============================================ "
+
+start_of_program = "Starting the Dice Game Program ..... \n"
+end_of_program = "Ending the Dice Game Program ..... \n"
+exit_game_str = "Exit Program"
+game_history_str = "View Game History"
+
+main_menu_input = "Select any one of the above options: "
+main_menu_invalid_input = "Please select a valid value!"
+main_menu_opt_1 = "Press 1 to start the game"
+main_menu_opt_2 = "Press 2 to view game history"
+main_menu_opt_3 = "Press 3 to terminate the program"
+
+game_menu_input_1 = "Enter number of dice: "
+game_menu_input_2 = "Enter type of dice: "
+game_menu_invalid_input_1 = "Please enter values: 6, 8 or 9."
+game_menu_invalid_input_2 = "Please enter values between 2 to 6."
+game_menu_opt_1 = "Press 1 to roll the dice"
+game_menu_opt_2 = "Press 2 to check win/lose"
+game_menu_opt_3 = "Press 3 to exit the game"
+
+change_dice_type_input = "Enter yes, if you wish to change dice type or enter no: "
+change_dice_type_invalid_input_1 = "Please enter yes or no."
+change_dice_type_invalid_input_2 = "Please enter values: 6, 8 or 9."
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
+import game_data as data
 
+# =================== Part I: Required Functions - Starts ===================
+
+# Input: N/A
+# Output: N/A
 # This function initiates the game logic, and displays all the menus
 def main_menu():
     # selected value of main menu function
@@ -10,41 +15,45 @@ def main_menu():
     dice_result_list = list()
 
     while (not valid_main_menu_option(main_menu_option)):
-        print("============================================ New Game ============================================ ")
-        print("Press 1 to start the game")
-        print("Press 2 to view game history")
-        print("Press 3 to terminate the program")
+        print(data.new_game_string)
+        print(data.main_menu_opt_1)
+        print(data.main_menu_opt_2)
+        print(data.main_menu_opt_3)
         print("\n")
 
-        main_menu_option = int(input("Select any one of the above options: "))
+        main_menu_option = int(input(data.main_menu_input))
         if main_menu_option == 3:
-            print("Exit Program")
+            print(data.exit_game_str)
         elif main_menu_option == 2:
-            print("View Game History")
+            print(data.game_history_str)
             game_history_menu(game_result_list)
         elif main_menu_option == 1:
-            print("============================================ Start Game ============================================ ")
+            print(data.start_game_str)
             game_result_list.append(game_menu())
-            print("============================================ End Game   ============================================ ")
+            print(data.end_game_str)
         else:
-            print("Please select a valid value!")
+            print(data.main_menu_invalid_input)
 
+# Input: N/A
+# Output: List - Tuple - int
 # This function is the main game mennu
 def game_menu():
     number_of_dice = 0
     while (not valid_dice_count(number_of_dice)):
-        number_of_dice = int(input("Enter number of dice: "))
+        number_of_dice = int(input(data.game_menu_input_1))
         if valid_dice_count(number_of_dice):
             type_of_dice = 0
             while (not valid_type_of_dice(type_of_dice)):
-                type_of_dice = int(input("Enter type of dice: "))
+                type_of_dice = int(input(data.game_menu_input_2))
                 if (valid_type_of_dice(type_of_dice)):
                     return game_play(number_of_dice, type_of_dice)
                 else:
-                    print("Please enter values: 6, 8 or 9.")
+                    print(data.game_menu_invalid_input_1)
         else:
-            print("Please enter values between 2 to 6.")
+            print(data.game_menu_invalid_input_2)
 
+# Input: int, int
+# Output: Tuple - int
 # This function rolls the selected dice type selected number of times
 def roll_dice(number_of_dice, type_of_dice):
     outcome = list()
@@ -53,8 +62,13 @@ def roll_dice(number_of_dice, type_of_dice):
     display_dice_results(tuple(outcome))
     return tuple(outcome)
 
+# Input: List - Tuple - int
+# Output: String
 # This function checks if the player has won or lost the game
 def check_win_lose(dice_result_list):
+    if (len(dice_result_list) < 0):
+        return 'not played'
+
     if (len(dice_result_list) > 10):
         return 'lose'
 
@@ -72,6 +86,8 @@ def check_win_lose(dice_result_list):
     else:
         return 'lose'
 
+# Input: List - Tuple - int
+# Output: N/A
 # This function displays game history data
 def game_history_menu(game_result_list):
     game_history = list()
@@ -80,11 +96,17 @@ def game_history_menu(game_result_list):
 
     display_history(game_history)
 
+# =================== Part I: Required Functions - Ends =====================
+
+# =================== Part II: Addtional Functions - Start ==================
+
+# Input: int, int
+# Output: List - Tuple - int
 # This method executes main game play logic
 def game_play(number_of_dice, type_of_dice):
-    print("Press 1 to roll the dice")
-    print("Press 2 to check win/lose")
-    print("Press 3 to exit the game")
+    print(data.game_menu_opt_1)
+    print(data.game_menu_opt_2)
+    print(data.game_menu_opt_3)
     print("\n")
 
     # result for dice rolls in each game
@@ -101,28 +123,28 @@ def game_play(number_of_dice, type_of_dice):
         elif selected_option == 3:
             return dice_result_list
 
+# Input: int
+# Output: int
 # This method changes the value of dice type if the player opts to change the type of dice
 def change_dice_type(type_of_dice):
     change_dice_type = ""
     while (not valid_response(change_dice_type)):
-        change_dice_type = input("Enter yes, if you wish to change dice type or enter no.")
+        change_dice_type = input(data.change_dice_type_input)
         if change_dice_type == "yes":
             new_type_of_dice = 0
             while (not valid_type_of_dice(new_type_of_dice)):
-                new_type_of_dice = int(input("Enter type of dice: "))
+                new_type_of_dice = int(input(data.game_menu_input_2))
                 if (valid_type_of_dice(new_type_of_dice)):
                     return new_type_of_dice
                 else:
-                    print("Please enter values: 6, 8 or 9.")
+                    print(data.change_dice_type_invalid_input_2)
         elif change_dice_type == "no":
             return type_of_dice
         else:
-            print("Please enter yes or no.")
+            print(data.change_dice_type_invalid_input_1)
 
-# This method checks if the player wants to exit the game
-def exit_game_option(selected_option):
-    return (selected_option == 3)
-
+# Input: int
+# Output: int
 # This function returns the value after the roll of a dice
 def roll_value(type_of_dice):
     if type_of_dice == 6:
@@ -132,6 +154,9 @@ def roll_value(type_of_dice):
     elif type_of_dice == 9:
         return random.choice([1,1,1,1,5,9])
 
+# Input: List
+# Output: int
+# This function calculates the median of the of the game
 def calculate_median(list_of_numbers):
     size_is_even = ((len(list_of_numbers)/2) % 2) == 0
     middle_of_list = int(len(list_of_numbers)/2)
@@ -140,21 +165,8 @@ def calculate_median(list_of_numbers):
     else:
         return int(list_of_numbers[middle_of_list])
 
-# This function checks validity of main menu options
-def valid_main_menu_option(main_menu_option):
-    return (main_menu_option == 3)
-
-# This function checks validity of dice count
-def valid_dice_count(number_of_dice):
-    return (number_of_dice >= 2 and number_of_dice <= 6)
-
-# This function checks validity of dice type
-def valid_type_of_dice(type_of_dice):
-    return (type_of_dice == 6 or type_of_dice == 8 or type_of_dice == 9)
-
-def valid_response(response):
-    return (response == "yes" or response == "no")
-
+# Input: List
+# Output: N/A
 # This function displays the result of selected number of dices after each roll
 def display_dice_results(game_turn):
     rows = []
@@ -162,7 +174,9 @@ def display_dice_results(game_turn):
         rows.append(dice(outcome))
     display_dice(rows)
 
-# This function returns the display of dice based of number generated after a roll
+# Input: int
+# Output: 2-D Array
+# This function returns the display of dice based of number generated after a roll in 2-D Array
 def dice(outcome):
     display = []
     if outcome == 1:
@@ -223,6 +237,8 @@ def dice(outcome):
         "-------")
     return display[0]
 
+# Input: 2-D Array
+# Output: N/A
 # This function prints the final result
 def display_dice(rows):
     for rx in range(5):
@@ -230,6 +246,8 @@ def display_dice(rows):
           print(ry[rx*7:(rx+1)*7], end='          ')
        print()
 
+# Input: List - Dictionary
+# Output: N/A
 # This method display game history in string format
 def display_history(game_history):
     game_history = sorted(game_history, key=lambda i: i['number_of_roll'])
@@ -238,6 +256,48 @@ def display_history(game_history):
         result_display_string += "Dice rolling times: " + str(game['number_of_roll']) + ", Number of dice: " + str(game['number_of_dice']) + ", Result: " + game['result'] + "\n"
     print(result_display_string)
 
-print("Starting the Dice Game ..... \n")
+# =================== Part II: Addtional Functions - Ends ===================
+
+# =================== Part III: Validation Functions Starts =================
+
+# Input: int
+# Output: boolean
+# This method checks if the player wants to exit the game
+def exit_game_option(selected_option):
+    return (selected_option == 3)
+
+# Input: int
+# Output: boolean
+# This function checks validity of main menu options
+def valid_main_menu_option(main_menu_option):
+    return (main_menu_option == 3)
+
+# Input: int
+# Output: boolean
+# This function checks validity of dice count
+def valid_dice_count(number_of_dice):
+    return (number_of_dice >= 2 and number_of_dice <= 6)
+
+# Input: int
+# Output: boolean
+# This function checks validity of dice type
+def valid_type_of_dice(type_of_dice):
+    return (type_of_dice == 6 or type_of_dice == 8 or type_of_dice == 9)
+
+# Input: String
+# Output: boolean
+# This function checks validity of response
+def valid_response(response):
+    return (response == "yes" or response == "no")
+
+# =================== Part III: Validation Functions Ends ===================
+
+# =================== Part IV: Main Program Starts ==========================
+
+print(data.start_of_program)
 print()
 main_menu()
+print()
+print(data.end_of_program)
+
+# =================== Part IV: Main Program End =============================
